ff90e3575760238a6fb0f8e30f4a50d63b691913
Tunjimoono <Tunjimoono>
2022-09-14 15:46:53 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
 #!/bin/bash
-printf "Programming is like building a multilingual puzzle\n"
+#!/bin/nclude <stdio.h>
+
+
+
+int main(void)
+	
+{
+	
+		puts("Programming is like building a multilingual puzzle\n");
+		
+			return (0);
+			
+}bash
